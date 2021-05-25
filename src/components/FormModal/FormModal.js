c098e601f6a8da4586856dcde541789b7b939264
Aleksandar Veljkovic <1eyedea2eyedeas@gmail.com>
2021-05-25 09:44:51 +0200

    Add useEffect hook and individual validation

--- src/components/FormModal/FormModal.js
+++ src/components/FormModal/FormModal.js
@@ -1,37 +1,73 @@
-import React, { useState, Fragment } from 'react';
+import React, { useState, Fragment, useEffect } from 'react';
 import styles from './style/FormModal.module.css';
 
 const FormModal = (props) => {
     const [enteredName, setEnteredName] = useState('');
     const [enteredURL, setEnteredURL] = useState('');
     const [isValid, setIsValid] = useState(true);
+    const [isEnteredNameValid, setIsEnteredNameValid] = useState(true);
+    const [isEnteredURLValid, setIsEnteredURLValid] = useState(true);
+
+    useEffect(() => {
+        const indetifier = setTimeout(() => {
+            if (!isEnteredNameValid || !isEnteredURLValid) {
+                setIsValid(false);
+            } else {
+                setIsValid(true);
+            }
+        }, 300);
+
+        return () => {
+            clearTimeout(indetifier);
+        };
+    }, [isEnteredNameValid, isEnteredURLValid, isValid]);
 
     const addNewUser = (event) => {
         event.preventDefault();
-        if (enteredName.trim().length === 0 || enteredURL.trim().length === 0) {
-            setIsValid(false);
+        if (!enteredName) {
+            setIsEnteredNameValid(false);
             return;
         }
-        props.getNewUserData(enteredName, enteredURL);
-        setEnteredName('');
-        setEnteredURL('');
-        props.hideForm();
+
+        if (!enteredURL) {
+            setIsEnteredURLValid(false);
+            return;
+        }
+
+        if (!isValid) {
+            return;
+        } else {
+            props.getNewUserData(enteredName, enteredURL);
+            setEnteredName('');
+            setEnteredURL('');
+            props.hideForm();
+        }
     };
 
     const enterNameHandler = (event) => {
         setEnteredName(event.target.value);
+        if (event.target.value.trim().length === 0) {
+            setIsEnteredNameValid(false);
+        } else {
+            setIsEnteredNameValid(true);
+        }
     };
     const enterURLHandler = (event) => {
         setEnteredURL(event.target.value);
+        if (!event.target.value.includes('https')) {
+            setIsEnteredURLValid(false);
+        } else {
+            setIsEnteredURLValid(true);
+        }
     };
 
     return (
         <Fragment>
             <form action="submit" onSubmit={addNewUser} className={styles.form}>
-                <div className="actual-input">
+                <div className={styles['actual-input']}>
                     <label
                         htmlFor="name"
-                        className={`${!isValid && styles.error}`}
+                        className={`${!isEnteredNameValid && styles.error}`}
                     >
                         Your name
                     </label>
@@ -39,13 +75,16 @@ const FormModal = (props) => {
                         type="text"
                         value={enteredName}
                         onChange={enterNameHandler}
-                        className={`${!isValid && styles.error}`}
+                        className={`${!isEnteredNameValid && styles.error}`}
                     />
+                    {!isEnteredNameValid && (
+                        <p className={styles.proba}>Please enter your name</p>
+                    )}
                 </div>
-                <div className="actual-input">
+                <div className={styles['actual-input']}>
                     <label
-                        htmlFor="name"
-                        className={`${!isValid && styles.error}`}
+                        htmlFor="url"
+                        className={`${!isEnteredURLValid && styles.error}`}
                     >
                         URL to your picture
                     </label>
@@ -53,8 +92,13 @@ const FormModal = (props) => {
                         type="text"
                         value={enteredURL}
                         onChange={enterURLHandler}
-                        className={`${!isValid && styles.error}`}
+                        className={`${!isEnteredURLValid && styles.error}`}
                     />
+                    {!isEnteredURLValid && (
+                        <p className={styles.proba}>
+                            URL must begin with https
+                        </p>
+                    )}
                 </div>
                 <button type="submit">Add</button>
                 <button onClick={props.hideForm}>Cancel</button>
