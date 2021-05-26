b7a29dff36106e5d79fa5bf14c1ad8846685bae4
Aleksandar Veljkovic <1eyedea2eyedeas@gmail.com>
2021-05-26 15:02:07 +0200

    Add useReducer

--- src/components/FormModal/FormModal.js
+++ src/components/FormModal/FormModal.js
@@ -1,65 +1,89 @@
-import React, { useState, Fragment, useEffect } from 'react';
+import React, { useState, Fragment, useEffect, useReducer } from 'react';
 import styles from './style/FormModal.module.css';
 
+const nameReducer = (state, action) => {
+    if (action.type === 'USER_INPUT') {
+        return { value: action.val, isValid: action.val.trim().length > 2 };
+    }
+    if (action.type === 'INPUT_BLUR') {
+        return { value: state.val, isValid: false };
+    }
+    return { val: '', isValid: false };
+};
+const urlReducer = (state, action) => {
+    if (action.type === 'USER_INPUT') {
+        return { value: action.val, isValid: action.val.includes('https') };
+    }
+    if (action.type === 'INPUT_BLUR') {
+        return { value: state.val, isValid: state.val.includes('https') };
+    }
+    return { val: '', isValid: false };
+};
+
 const FormModal = (props) => {
-    const [enteredName, setEnteredName] = useState('');
-    const [enteredURL, setEnteredURL] = useState('');
-    const [isValid, setIsValid] = useState(true);
-    const [isEnteredNameValid, setIsEnteredNameValid] = useState(true);
-    const [isEnteredURLValid, setIsEnteredURLValid] = useState(true);
+    // const [enteredName, setEnteredName] = useState('');
+    // const [enteredURL, setEnteredURL] = useState('');
+    // const [isEnteredNameValid, setIsEnteredNameValid] = useState(true);
+    // const [isEnteredURLValid, setIsEnteredURLValid] = useState(true);
+
+    const [isFormValid, setIsFormValid] = useState(null);
+
+    const [nameState, dispatchName] = useReducer(nameReducer, {
+        value: '',
+        isValid: null,
+    });
+    const [urlState, dispatchUrl] = useReducer(urlReducer, {
+        value: '',
+        isValid: null,
+    });
+
+    let { isValid: isEnteredNameValid } = nameState;
+    let { isValid: isEnteredURLValid } = urlState;
 
     useEffect(() => {
         const indetifier = setTimeout(() => {
-            if (!isEnteredNameValid || !isEnteredURLValid) {
-                setIsValid(false);
-            } else {
-                setIsValid(true);
-            }
-        }, 300);
+            console.log(isEnteredNameValid, isEnteredURLValid);
+            setIsFormValid(isEnteredNameValid && isEnteredURLValid);
+        }, 200);
 
         return () => {
             clearTimeout(indetifier);
         };
-    }, [isEnteredNameValid, isEnteredURLValid, isValid]);
+    }, [isEnteredNameValid, isEnteredURLValid, isFormValid]);
 
     const addNewUser = (event) => {
         event.preventDefault();
-        if (!enteredName) {
-            setIsEnteredNameValid(false);
-            return;
-        }
 
-        if (!enteredURL) {
-            setIsEnteredURLValid(false);
-            return;
-        }
-
-        if (!isValid) {
+        if (!isFormValid) {
+            if (isEnteredNameValid === null && isEnteredURLValid === null) {
+                console.log('Nisi nista napisao');
+            }
             return;
         } else {
-            props.getNewUserData(enteredName, enteredURL);
-            setEnteredName('');
-            setEnteredURL('');
+            props.getNewUserData(nameState.value, urlState.value);
             props.hideForm();
         }
     };
 
     const enterNameHandler = (event) => {
-        setEnteredName(event.target.value);
-        if (event.target.value.trim().length === 0) {
-            setIsEnteredNameValid(false);
-        } else {
-            setIsEnteredNameValid(true);
-        }
+        // setEnteredName(event.target.value);
+
+        dispatchName({
+            type: 'USER_INPUT',
+            val: event.target.value,
+        });
     };
+
+    // const validateNameHandler = () => {
+    //     dispatchName({ type: 'INPUT_BLUR' });
+    // };
+
     const enterURLHandler = (event) => {
-        setEnteredURL(event.target.value);
-        if (!event.target.value.includes('https')) {
-            setIsEnteredURLValid(false);
-        } else {
-            setIsEnteredURLValid(true);
-        }
+        dispatchUrl({ type: 'USER_INPUT', val: event.target.value });
     };
+    // const validateUrlHandler = () => {
+    //     dispatchUrl({ type: 'INPUT_BLUR' });
+    // };
 
     return (
         <Fragment>
@@ -67,34 +91,44 @@ const FormModal = (props) => {
                 <div className={styles['actual-input']}>
                     <label
                         htmlFor="name"
-                        className={`${!isEnteredNameValid && styles.error}`}
+                        className={`${
+                            isEnteredNameValid === false ? styles.error : ''
+                        }`}
                     >
                         Your name
                     </label>
                     <input
                         type="text"
-                        value={enteredName}
+                        value={nameState.value}
                         onChange={enterNameHandler}
-                        className={`${!isEnteredNameValid && styles.error}`}
+                        // onBlur={validateNameHandler}
+                        className={`${
+                            isEnteredNameValid === false ? styles.error : ''
+                        }`}
                     />
-                    {!isEnteredNameValid && (
+                    {isEnteredNameValid === false && (
                         <p className={styles.proba}>Please enter your name</p>
                     )}
                 </div>
                 <div className={styles['actual-input']}>
                     <label
                         htmlFor="url"
-                        className={`${!isEnteredURLValid && styles.error}`}
+                        className={`${
+                            urlState.isValid === false ? styles.error : ''
+                        }`}
                     >
                         URL to your picture
                     </label>
                     <input
                         type="text"
-                        value={enteredURL}
+                        value={urlState.value}
                         onChange={enterURLHandler}
-                        className={`${!isEnteredURLValid && styles.error}`}
+                        // onBlur={validateUrlHandler}
+                        className={`${
+                            urlState.isValid === false ? styles.error : ''
+                        }`}
                     />
-                    {!isEnteredURLValid && (
+                    {urlState.isValid === false && (
                         <p className={styles.proba}>
                             URL must begin with https
                         </p>
